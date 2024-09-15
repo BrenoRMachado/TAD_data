2c3802f2187e1682d0fce7b7bcd8fd649407172a
Bereno <burracha.6464@gmail.com>
2024-09-15 15:09:41 -0300

    atualiza a classe Data com atributos, Construtores e Destrutor + metódos setters

--- TAD Data/Data.h
+++ TAD Data/Data.h
@@ -1,7 +1,17 @@
 class Data 
 {
     private:
-
+        int dia, mes, ano;
+        int numDiasMes [12] = {0,31,59,90,120,151,181,212,243,273,304,334}; // Quantidade de dias transcorridos até o més anterior
     public:
-    
+        Data ( int d , int m, int a);
+        Data ();
+        ~Data ();
+
+        // setters para dia, mes e ano
+        void setDia();
+        void setMes();
+        void setAno();
+        
+
 };
